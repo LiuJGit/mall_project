b271b053078609670b01c00e4c85bc44a2515fa3
lj <jl@coremail.cn>
2023-10-29 02:21:22 +0800

    实现用户名重复注册检测前后端逻辑

--- my_mall/apps/users/views.py
+++ my_mall/apps/users/views.py
@@ -74,3 +74,16 @@ class RegisterView(View):
         # return redirect('/')
         # reverse('contents:index') == '/'
         return redirect(reverse('contents:index'))
+    
+
+class UsernameCountView(View):
+    """判断用户名是否重复注册"""
+
+    def get(self, request, username):
+        """
+        :param request: 请求对象
+        :param username: 用户名
+        :return: JSON
+        """
+        count = User.objects.filter(username=username).count()
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'count': count})
